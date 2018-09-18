fb0070ce19ad1592f01fd1ddd40db848acfc979c
Maxime Golfier <maxgolfier@gmail.com>
2018-09-18 17:27:33 +0200

    first commit

---
+++ app/src/main/java/com/example/golfier/android_youtube_api/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.golfier.android_youtube_api;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    MyUploads.retrieve();
+  }
+}
